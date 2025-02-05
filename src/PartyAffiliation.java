01d9ccb7b372b272fbbdf48c6f84a77e1d6e4904
mzerhusen <zerhusma@mail.uc.edu>
2025-02-05 14:57:17 -0500

    Initial commit

---
+++ src/PartyAffiliation.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PartyAffiliation {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String userAffiliation;
+        System.out.println("Thank you for sharing your information. Please select your party affiliation below: \n Democrat - D \t Republican - R \t Independent - I");
+        System.out.println("Your selection for party affiliation is: ");
+        userAffiliation = input.nextLine();
+        userAffiliation = userAffiliation.toUpperCase();
+        if(userAffiliation.equals("D"))
+        {
+            System.out.println("You selected Democrat, you get a donkey!");
+        }
+        else if (userAffiliation.equals("R"))
+        {
+            System.out.println("You selected Republican, you get an elephant!");
+        }
+        else if (userAffiliation.equals("I"))
+        {
+            System.out.println("You selected Independent, you get a person avatar!");
+        }
+        else
+        {
+            System.out.println("You selected " + userAffiliation + " your imagination determines what you get!");
+        }
+    }
+}
